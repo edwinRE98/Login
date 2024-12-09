7e381a6d86f894c7832ef7a659a58295de1b619b
Edwin Ropain E. <edwinropain260@outlook.com>
2024-12-09 00:15:45 -0500

    Prueba de conexión

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -11,6 +11,6 @@ package com.mycompany.login;
 public class Login {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
