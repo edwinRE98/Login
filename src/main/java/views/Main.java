765c8211f1909321751584000828b5b4d5534ebc
Edwin Ropain E. <edwinropain260@outlook.com>
2024-12-09 00:47:14 -0500

    Creacion de la interfaz del login

---
+++ src/main/java/views/Main.java
@@ -0,0 +1,18 @@
+/*
+ *Este es un login básico conectado a una base de datos,
+  donde si este evalúa que si el usuario y la contraseña
+  que están en la base de datos son correctas,
+  entonces nos mostrará una ventana de bienvenida.
+ */
+package views;
+
+/**
+ *
+ * @author Edwin Ropain Espinosa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
